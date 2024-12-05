fa1496d485429710d667d7c3746bf5ac4f3e24e8
henriquedevop <wymuxgh@gmail.com>
2024-12-05 18:15:32 -0300

    feat: create global context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router"
 import { router } from "./routes/router"
+import CartProvider from "./contexts/CartContext"
 
 function App() {
   return (
     <>
-    <RouterProvider router={router}/>
+    <CartProvider>
+      <RouterProvider router={router}/>
+    </CartProvider>
     </>
   )
 }
