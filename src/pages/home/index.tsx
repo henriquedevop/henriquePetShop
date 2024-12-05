b061b2854de16b2d526fce33916b559df2334496
henriquedevop <wymuxgh@gmail.com>
2024-12-05 17:30:05 -0300

    feat: get api data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
+
+interface ProductProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string
+}
+
 export function Home() {
+
+    const [products, setProducts] = useState<ProductProps[]>([])
+
+    useEffect(() => {
+        async function getData() {
+            const response = await api.get("/products")
+            setProducts(response.data)
+        }
+
+        getData()
+    },[])
+
     return (
-        <div>Home</div>
+        <div>
+            
+        </div>
     )
 }
