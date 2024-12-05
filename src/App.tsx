03d128bafd9ffa6833067f29bb4105d163e9264c
henriquedevop <wymuxgh@gmail.com>
2024-12-05 17:25:30 -0300

    feat: configure routes, outlet, layout and create header compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { useEffect, useState } from "react"
-import { api } from "./services/api"
-
-interface ProductsProps {
-  id: number;
-  title: string;
-  description: string;
-  price: number;
-  cover: string
-}
+import { RouterProvider } from "react-router"
+import { router } from "./routes/router"
 
 function App() {
-
-  const [products, setProducts] = useState<ProductsProps[]>([])
-
-  useEffect(() => {
-    async function getData() {
-      const response = await api.get("/products")
-      setProducts(response.data)
-    }
-    getData()
-  },[])
-
   return (
-    <div className="w-full h-screen bg-black">
-      <h1 className="text-white">teste</h1>
-    </div>
+    <>
+    <RouterProvider router={router}/>
+    </>
   )
 }
 
---
+++ src/routes/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+
+import { Home } from "../pages/home";
+import { Cart } from "../pages/cart";
+import { Layout } from "../components/layout";
+
+const router = createBrowserRouter([
+    {element: <Layout/>, children: [
+        {path: "/", element: <Home/>},
+        {path: "/cart", element: <Cart/>},
+    ]}
+])
+
+export { router }
