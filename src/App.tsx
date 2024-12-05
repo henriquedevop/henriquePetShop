ae616a91504c121a53be06e948140ff092354222
henriquedevop <wymuxgh@gmail.com>
2024-12-05 11:26:00 -0300

    feat: fetch api data and set on useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react"
+import { api } from "./services/api"
+
+interface ProductsProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string
+}
+
 function App() {
+
+  const [products, setProducts] = useState<ProductsProps[]>([])
+
+  useEffect(() => {
+    async function getData() {
+      const response = await api.get("/products")
+      setProducts(response.data)
+    }
+    getData()
+  },[])
 
   return (
     <div className="w-full h-screen bg-black">
