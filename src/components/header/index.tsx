14b3607a2032212d6446c5fc22d274704faef900
henriquedevop <wymuxgh@gmail.com>
2024-12-05 18:18:25 -0300

    feat: import context on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,12 @@
 import { BiCart } from "react-icons/bi"
 import { Link } from "react-router"
+import { useContext } from "react"
+import { CartContext } from "../../contexts/CartContext"
 
 export function Header() {
+
+    const {cartAmount} = useContext(CartContext)
+
     return (
         <header className="bg-gradient-to-r from-orange-400 to-yellow-300 text-white shadow-md">
         <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
@@ -14,7 +19,9 @@ export function Header() {
           >
             <BiCart size={28} />
             <span className="hidden sm:inline text-lg font-medium">Carrinho</span>
-              <span className="absolute -top-2 -right-3 bg-red-500 text-white text-sm font-bold rounded-full h-5 w-5 flex items-center justify-center">1</span>
+            {cartAmount > 0 && (
+            <span className="absolute -top-2 -right-3 bg-red-500 text-white text-sm font-bold rounded-full h-5 w-5 flex items-center justify-center">{cartAmount}</span>
+            )}
           </Link>
         </div>
       </header>
